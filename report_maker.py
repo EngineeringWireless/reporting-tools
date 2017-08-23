12fb8e1956960f886084127752f6a6ed90282748
Ted Metcalfe <ted.metcalfe@engineeringwireless.com>
2017-08-23 14:12:48 -0700

    moved insert_image to prs_tools since it is not an object specific task.

--- report_maker.py
+++ report_maker.py
@@ -7,19 +7,22 @@ from utils.image_tools import fit_image
 class Report():
 
     '''
-    creates a class containing a pptx presentation used to create the report.
+    Creates a class containing a pptx presentation used to create the report.
     '''
     def __init__(self):
 
         self.prs = Presentation('tools/PPTX_TEMPLATE.pptx')
 
     '''
-    adds a "Plot with Legend" slide to the presentationa and returns that slide
-    to be worked with.
+    Adds a "Plot with Legend" slide to the presentationa and returns that slide
+    to be worked with.  Returns a dictionary with the slide object and all of
+    that slides placeholder objects with their names as the key.
     '''
     def add_image_slide(self):
+
         img_layout = 'Plot with Legend'
         slide_info = {}
+
         self.cur_slide = self.prs.slides.add_slide(
                                 get_layout(self.prs.slide_layouts, img_layout)
                                 )
@@ -32,21 +35,9 @@ class Report():
 
         return slide_info
 
-    def insert_image(self, placeholder, image):
-
-        size = [
-            int(Length(placeholder.width).pt),
-            int(Length(placeholder.height).pt)
-        ]
-
-        img = fit_image(image, size)
-
-        img_placeholder = placeholder.insert_picture(img)
-
-        return img_placeholder
 
     '''
-    saves presentation to given location.
+    Saves presentation to given location.
     '''
     def save(self, file_name):
         self.prs.save(file_name)
--- utils/prs_tools.py
+++ utils/prs_tools.py
@@ -9,3 +9,22 @@ def get_layout(layouts, layout_name):
             return layout
         else:
             return None
+
+'''
+Inserts and image into the passed placeholder.  Checks the size of the
+placeholder and uses fit_image to make sure the entire image will be
+displayed.  Since PPTX changes the object after the image is inserted,
+this will return that new object.
+'''
+def insert_image(self, placeholder, image):
+
+    size = [
+        int(Length(placeholder.width).pt),
+        int(Length(placeholder.height).pt)
+    ]
+
+    img = fit_image(image, size)
+
+    img_placeholder = placeholder.insert_picture(img)
+
+    return img_placeholder
